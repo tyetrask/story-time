4dfbd2e5bd98bb20336c2eb2effa2e930a06515a
Tye Trask <inc3pt@gmail.com>
2014-10-18 20:54:31 +0000

    page controler, root route;

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 StoryTime::Application.routes.draw do
+  get "page/index"
   devise_for :users
   resources :users
+  
+  root :to => "page#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
